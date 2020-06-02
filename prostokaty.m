ff8fe1da619cb29208bbc71d383acc4f70a2b0f8
Karol Dzialowski <karlososhd@gmail.com>
2020-06-02 18:59:35 +0200

    Refactor prostokątów

--- prostokaty.m
+++ prostokaty.m
@@ -22,91 +22,59 @@ z2 = 191;
 z3 = 210;
 z4 = 273;
 
+% objetosc
+suma = 0;
 
 % Trapez zielony
-szerokoscTrapezu = x4;
-h = z4;
-a = y2;
-b = y4;
-ob1 = (((a+b)/2)*h)*szerokoscTrapezu;
-    
-    suma = 0;
-    for x = x1 : 25 : x4
-        for y = y1 : 25 : y2
-            suma = suma + z2;
-            plot3([x,x], [y,y], [z1,z4],'g');
-            plot3([x,x], [y,y], [z1,z4],'r.');
-
-        end
+for x = x1 : 25 : x4
+    for y = y1 : 25 : y2
+        suma = suma + z4;
+        plot3([x,x], [y,y], [z1,z4],'g');
+        plot3([x,x], [y,y], [z1,z4],'r.');
     end
+end
 
-    % Trapez zielony
-szerokoscTrapezu = x4;
-h = z4;
-a = y2;
-b = y4;
-ob1 = (((a+b)/2)*h)*szerokoscTrapezu;
-    
-    suma = 0;
-    for x = x1 : 25 : x4
-        for y = y1 : 25 : y2
-            suma = suma + z4;
-            plot3([x,x], [y,y], [z1,z4],'g');
-            plot3([x,x], [y,y], [z1,z4],'r.');
-
-        end
+% Trapez zielony
+for x = x1 : 25 : x4
+    for y = y1 : 25 : y2
+        suma = suma + z4;
+        plot3([x,x], [y,y], [z1,z4],'g');
+        plot3([x,x], [y,y], [z1,z4],'r.');
     end
+end
  
-szerokoscTrapezu = x3-x2;
-h = z3;
-a = y5 - y4;
-b = a;
-ob2 = (((a+b)/2)*h)*szerokoscTrapezu;
+
 [c, d] = meshgrid([x1,x4], [y2,y4]);
 zz = [z4 z4; z2 z2];
 
-    suma2 = 0;
-    for x = x1 : 25 : x4
-        for y = y2 : 25 : y4
-            suma2 = suma2 + z2;
-            plot3([x,x], [y,y], [z1,interp2(c, d, zz, x, y)],'g');
-            plot3([x,x], [y,y], [z1,interp2(c, d, zz, x, y)],'r.');
-
-        end
+for x = x1 : 25 : x4
+    for y = y2 : 25 : y4
+        suma = suma + interp2(c, d, zz, x, y);
+        plot3([x,x], [y,y], [z1,interp2(c, d, zz, x, y)],'g');
+        plot3([x,x], [y,y], [z1,interp2(c, d, zz, x, y)],'r.');
     end
+end
     
-    % purpurowy
-    szerokoscTrapezu = x3-x2;
-h = z3 - z2;
-a = y5 - y3;
-b = y5 - y4;
-ob3 = (((a+b)/2) * h) * szerokoscTrapezu;
+% purpurowy
 [c, d] = meshgrid([x2,x3], [y3,y4,y5]);
 zz = [z3 z3; z2 z2; z2 z2];
-    suma3 = 0;
-    for x = x2 : 25 : x3
-        for y = y3 : 25 : y5
-            suma3 = suma3 + z2;
-            plot3([x,x], [y,y], [interp2(c, d, zz, x, y), z3],'g');
-            plot3([x,x], [y,y], [interp2(c, d, zz, x, y), z3],'r.');
-        end
+for x = x2 : 25 : x3
+    for y = y3 : 25 : y5
+        suma = suma + (z3 - interp2(c, d, zz, x, y));
+        plot3([x,x], [y,y], [interp2(c, d, zz, x, y), z3],'g');
+        plot3([x,x], [y,y], [interp2(c, d, zz, x, y), z3],'r.');
     end
+end
     
     
-    %czerwony
-    szerokoscTrapezu = x3-x2;
-h = z3 - z2;
-a = y5 - y3;
-b = y5 - y4;
-ob3 = (((a+b)/2) * h) * szerokoscTrapezu;
-    suma4 = 0;
-    for x = x2 : 25 : x3
-        for y = y4 : 25 : y5
-            suma4 = suma4 + z2;
-            plot3([x,x], [y,y], [z1,z2],'g');
-            plot3([x,x], [y,y], [z1,z2],'r.');
+%czerwony
+for x = x2 : 25 : x3
+    for y = y4 : 25 : y5
+        suma = suma + z2;
+        plot3([x,x], [y,y], [z1,z2],'g');
+        plot3([x,x], [y,y], [z1,z2],'r.');
+    end
+end
 
-        end
-    end
-    view(3);
+view(3);
     
