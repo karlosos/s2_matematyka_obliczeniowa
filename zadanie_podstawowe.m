f5851d30ad63e606a50c926e568dc21ba4e2d5fe
Karol Dzialowski <karlososhd@gmail.com>
2020-06-02 21:23:41 +0200

    Zmiana wspolrzednych

--- zadanie_podstawowe.m
+++ zadanie_podstawowe.m
@@ -4,20 +4,20 @@ close all
 
 % Zdefiniowanie punktow bryły
 x1 = 0;
-x2 = 385;
-x3 = 579;
-x4 = 655;
+x2 = 400;
+x3 = 600;
+x4 = 650;
 
 y1 = 0;
-y2 = 352;
-y3 = 417;
-y4 = 439;
-y5 = 572;
+y2 = 350;
+y3 = 400;
+y4 = 450;
+y5 = 600;
 
 z1 = 0;
-z2 = 191;
-z3 = 210;
-z4 = 273;
+z2 = 200;
+z3 = 220;
+z4 = 250;
 
 % Rysowanie bryly
 bryla;
@@ -45,10 +45,10 @@ h = z2;
 a = y5 - y4;
 b = a;
 ob2 = (((a+b)/2)*h)*szerokoscTrapezu;
-line([x3 x3], [y4 y5], [z1 z1], 'LineWidth', 2, 'color', 'r');
-line([x3 x3], [y4 y5], [z2 z2], 'LineWidth', 2, 'color', 'r');
-line([x3 x3], [y4 y4], [z1 z2], 'LineWidth', 2, 'color', 'r');
-line([x3 x3], [y5 y5], [z1 z2], 'LineWidth', 2, 'color', 'r');
+line([x3 x3], [y4 y5], [z1 z1], 'LineWidth', 2, 'color', 'g');
+line([x3 x3], [y4 y5], [z2 z2], 'LineWidth', 2, 'color', 'g');
+line([x3 x3], [y4 y4], [z1 z2], 'LineWidth', 2, 'color', 'g');
+line([x3 x3], [y5 y5], [z1 z2], 'LineWidth', 2, 'color', 'g');
 
 % Trapez purpurowy
 szerokoscTrapezu = x3-x2;
@@ -56,9 +56,9 @@ h = z3 - z2;
 a = y5 - y3;
 b = y5 - y4;
 ob3 = (((a+b)/2) * h) * szerokoscTrapezu;
-line([x3 x3], [y3 y4], [z3 z2], 'LineWidth', 2, 'color', 'm');
-line([x3 x3], [y3 y5], [z3 z3], 'LineWidth', 2, 'color', 'm');
-line([x3 x3], [y5 y5], [z3 z2], 'LineWidth', 2, 'color', 'm');
+line([x3 x3], [y3 y4], [z3 z2], 'LineWidth', 2, 'color', 'g');
+line([x3 x3], [y3 y5], [z3 z3], 'LineWidth', 2, 'color', 'g');
+line([x3 x3], [y5 y5], [z3 z2], 'LineWidth', 2, 'color', 'g');
 
 % Wynik
 Ob = ob1 + ob2 + ob3;
@@ -83,7 +83,7 @@ bryla
 hold on
 
 % Generowanie punktów losowych
-ilosc_punktow = 10000;
+ilosc_punktow = 50000;
 X = (rand(1, ilosc_punktow))*x4;
 Y = (rand(1, ilosc_punktow))*y5;
 Z = (rand(1, ilosc_punktow))*z4;
@@ -97,15 +97,17 @@ for i = 1:1:ilosc_punktow
         plot3(X(i),Y(i),Z(i), '*g');
         ile_trafionych = ile_trafionych + 1;
     elseif Z(i) <= z3 && Y(i) <= y5 && Y(i) >= y4 && X(i) >= x2 && X(i) <= x3
-        plot3(X(i),Y(i),Z(i), '*r');
+        plot3(X(i),Y(i),Z(i), '*g');
         ile_trafionych = ile_trafionych + 1;
     elseif Z(i) <= interp2([x4 0], [y2 y4], [z4 z4; z2 z2], X(i), Y(i), 'cubic')
-        plot3(X(i),Y(i),Z(i), '*m');
+        plot3(X(i),Y(i),Z(i), '*g');
         ile_trafionych = ile_trafionych + 1;
     else
-        plot3(X(i),Y(i),Z(i), '*k');
+        plot3(X(i),Y(i),Z(i), '*r');
     end
 end
+
+view(140, 35)
 
 % Obliczanie objętości
 wynik = (ile_trafionych/ilosc_punktow) * objetosc_przestrzeni;
